bd85159c4a7240d9ddb0dc5991d7e325a880c18f
Marcin Lech <arcinech@gmail.com>
2022-06-11 11:59:27 +0200

    Add setFavorite action creator and reducer case

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,9 +5,14 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+export const setFavorite = payload => ({ type: 'SET_FAVORITE', payload });
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case 'SET_FAVORITE':
+      return statePart.map(product =>
+        product.id === action.payload.id ? { ...product, ...action.payload } : product
+      );
     default:
       return statePart;
   }
