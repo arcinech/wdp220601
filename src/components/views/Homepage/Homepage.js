657e435527971751d9dc8b24ef43873a7d1ffa3e
Marcin Lech <arcinech@gmail.com>
2022-06-15 18:27:08 +0200

    Add: SectionDeals to homepage

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -3,10 +3,12 @@ import React from 'react';
 import styles from './Homepage.module.scss';
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+import SectionDeals from '../../layout/SectionDeals/SectionDeals';
 
 const Homepage = () => (
   <div className={styles.root}>
     <FeatureBoxes />
+    <SectionDeals />
     <NewFurniture />
   </div>
 );
