8d5e35f472f402e8f832f7ac0f764939cbc2e448
Marcin Lech <arcinech@gmail.com>
2022-06-16 21:13:40 +0200

    Add: redux logic for staticdeals

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,14 @@ import initialState from './initialState';
 import cartReducer from './cartRedux';
 import categoriesReducer from './categoriesRedux';
 import productsReducer from './productsRedux';
+import staticDealsReducer from './staticDealsRedux';
 
 // define reducers
 const reducers = {
   cart: cartReducer,
   categories: categoriesReducer,
   products: productsReducer,
+  deals: staticDealsReducer,
 };
 
 // add blank reducers for initial state properties without reducers
