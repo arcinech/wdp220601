b5df532f7a0ea5147e3bb1ba27e94f49fdbf2aff
Michal Plewa <themipsial@gmail.com>
2022-06-17 15:02:45 +0200

    add files

---
+++ src/components/features/Promotions/Promotions.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './Promotions.module.scss';
+
+const Promotions = () => {
+  return (
+    <div className={`${styles.root} container`}>
+      <div className='row no-gutters align-items-end'>
+        <div className='col-6'>dupa</div>
+        <div className='col-6'>dupa2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Promotions;
--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -3,9 +3,11 @@ import React from 'react';
 import styles from './Homepage.module.scss';
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
+import Promotions from '../../features/Promotions/Promotions';
 
 const Homepage = () => (
   <div className={styles.root}>
+    <Promotions />
     <FeatureBoxes />
     <NewFurniture />
   </div>
