2c61867de8bea33567b1fff47d66bac84b3d60a6
Michal Plewa <themipsial@gmail.com>
2022-06-17 14:07:06 +0200

    fix review

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -1,5 +1,5 @@
 /* selectors */
-export const getAllProducts = (state) => state.products;
+export const getAllProducts = state => state.products;
 export const getCountProducts = ({ products }) => products.length;
 
 export const getNewProducts = ({ products }) =>
@@ -16,9 +16,7 @@ const REMOVE_COMPARE = createActionName('REMOVE_COMPARE');
 
 /* action creators */
 export const setFavorite = payload => ({ type: SET_FAVORITE, payload });
-export const addCompare = payload => {
-  return { payload, type: ADD_COMPARE };
-};
+export const addCompare = payload => ({ payload, type: ADD_COMPARE });
 
 export const removeCompare = payload => {
   return { payload, type: REMOVE_COMPARE };
@@ -27,11 +25,11 @@ export const removeCompare = payload => {
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
-      case 'SET_FAVORITE':
+    case 'SET_FAVORITE':
       return statePart.map(product =>
         product.id === action.payload.id ? { ...product, ...action.payload } : product
       );
-      case ADD_COMPARE: {
+    case ADD_COMPARE: {
       return statePart.map(item => {
         if (item.id !== action.payload) return item;
         return {
