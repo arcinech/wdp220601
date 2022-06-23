bda3047667d47f87ece87d21cb188c3c1ba089a7
KatSobieska <97301917+KatSobieska@users.noreply.github.com>
2022-06-23 21:58:43 +0200

    Add Swipeable funcionality and import Brand component data based on initialState

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -1,5 +1,6 @@
 import { Button } from 'react-bootstrap';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './Brands.module.scss';
 
@@ -7,24 +8,72 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { getAllBrands } from '../../../redux/brandsRedux';
 import { useSelector } from 'react-redux/es/exports';
+import Brand from '../../common/Brand/Brand';
+import { useState, useCallback, useRef } from 'react';
+import {
+  Swiper,
+  SwiperSlide,
+} from '../../../../node_modules/swiper/react/swiper-react.js';
+import '../../../../node_modules/swiper/modules/grid/grid.scss';
+import '../../../../node_modules/swiper/swiper.scss';
+import '../../../../node_modules/swiper/modules/pagination/pagination.scss';
 
 const Brands = () => {
   const brands = useSelector(getAllBrands);
+
+  const [prevButton, setPrevButton] = useState(false);
+  const [nextButton, setNextButton] = useState(false);
+  const sliderRef = useRef(null);
+
+  const handlePrev = useCallback(e => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slidePrev();
+    if (e.type === 'mousedown') {
+      setPrevButton(true);
+    } else {
+      setPrevButton(false);
+    }
+  }, []);
+
+  const handleNext = useCallback(e => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slideNext();
+    if (e.type === 'mousedown') {
+      setNextButton(true);
+    } else {
+      setNextButton(false);
+    }
+  }, []);
 
   return (
     <div className={styles.root}>
       <div className='container'>
         <hr></hr>
-        <div className='row d-flex justify-content-around'>
-          <Button variant='dark'>
+        <div className='d-flex justify-content-around'>
+          <Button
+            variant='dark'
+            onClick={handlePrev}
+            className={prevButton && styles.active}
+          >
             <FontAwesomeIcon icon={faArrowLeft} />
           </Button>
-          {brands.map(brand => (
-            <div key={brand.id} className={`${styles.photo}`}>
-              <img src={`/images/Brand-${brand.id}.jpg`} alt='' />
-            </div>
-          ))}
-          <Button variant='dark'>
+          <Swiper
+            ref={sliderRef}
+            slidesPerView={6}
+            spaceBetween={30}
+            className='mySwiper'
+          >
+            {brands.map(brand => (
+              <SwiperSlide className='' key={brand.id}>
+                <Brand key={brand.id} id={brand.id} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <Button
+            variant='dark'
+            onClick={handleNext}
+            className={nextButton && styles.active}
+          >
             <FontAwesomeIcon icon={faArrowRight} />
           </Button>
         </div>
@@ -33,6 +82,10 @@ const Brands = () => {
       </div>
     </div>
   );
+};
+
+Brands.propTypes = {
+  id: PropTypes.number,
 };
 
 export default Brands;
