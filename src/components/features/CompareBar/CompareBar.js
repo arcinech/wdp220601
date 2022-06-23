7dd542c22c8b2a318955dcc234f54b154115f62e
Michal Plewa <themipsial@gmail.com>
2022-06-23 17:21:42 +0200

    add price, name, and favorit view

--- src/components/features/CompareBar/CompareBar.js
+++ src/components/features/CompareBar/CompareBar.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../common/Button/Button';
 import ProductImage from '../../common/ProductImage/ProductImage';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBan } from '@fortawesome/free-solid-svg-icons';
+import { faBan, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './CompareBar.module.scss';
-import { removeCompare } from '../../../redux/productsRedux';
+import { removeCompare, setFavorite } from '../../../redux/productsRedux';
 
 const CompareBar = () => {
-  const dispatche = useDispatch();
+  const dispatch = useDispatch();
   const compareProducts = useSelector(state =>
     state.products.filter(item => item.compare === true)
   );
   const handleClick = (e, id) => {
     e.preventDefault();
-    dispatche(removeCompare(id));
+    dispatch(removeCompare(id));
+  };
+  const [isFavorite, setIsFavorite] = useState(compareProducts.favorite || false);
+
+  const changeFavorite = e => {
+    e.preventDefault();
+    setIsFavorite(!isFavorite);
+    dispatch(setFavorite({ id: compareProducts.id, favorite: isFavorite }));
   };
   return (
     <nav className={styles.navbar}>
@@ -32,6 +39,25 @@ const CompareBar = () => {
                 </Button>
               </div>
               <ProductImage id={item.id} />
+              <div className='row'>
+                <div className={styles.name}>{item.name}</div>
+                <div className={styles.favorite}>
+                  <Button
+                    variant={isFavorite ? 'active' : 'outline'}
+                    onClick={changeFavorite}
+                  >
+                    <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+                  </Button>
+                </div>
+                <div className={item.oldPrice ? styles.priceBox : ''}>
+                  {item.oldPrice && <p>$ {item.oldPrice}</p>}
+                  <div className={styles.price}>
+                    <Button noHover variant='small'>
+                      $ {item.price}
+                    </Button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         ))}
