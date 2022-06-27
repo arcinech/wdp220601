0debff4999b9fe2a89f5cae70862b2cd5a4734df
Marcin Lech <arcinech@gmail.com>
2022-06-27 21:07:52 +0200

    Add: change of pages in slider

--- src/components/common/GallerySlider/GallerySlider.js
+++ src/components/common/GallerySlider/GallerySlider.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import styles from './GallerySlider.module.scss';
 import ProductImage from '../ProductImage/ProductImage';
@@ -6,18 +6,39 @@ import Button from '../Button/Button';
 import clsx from 'clsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import {
+  Swiper,
+  SwiperSlide,
+} from '../../../../node_modules/swiper/react/swiper-react.js';
+import '../../../../node_modules/swiper/swiper.scss';
+import '../../../../node_modules/swiper/modules/pagination/pagination.scss';
 
 const GalleryProduct = ({ setId, id, products }) => {
-  // products = useSelector(state => getAllProducts(state));
-  const [activePage, setActivePage] = useState(0);
+  const [prevButton, setPrevButton] = useState(false);
+  const [nextButton, setNextButton] = useState(false);
+  const sliderRef = useRef(null);
 
-  const leftClick = e => {
+  const leftClick = useCallback(e => {
     e.preventDefault();
-  };
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slidePrev(6);
+    if (e.type === 'mousedown') {
+      setPrevButton(true);
+    } else {
+      setPrevButton(false);
+    }
+  }, []);
 
-  const rightClick = e => {
+  const rightClick = useCallback(e => {
     e.preventDefault();
-  };
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slideNext(6);
+    if (e.type === 'mousedown') {
+      setNextButton(true);
+    } else {
+      setNextButton(false);
+    }
+  }, []);
 
   return (
     <div className={styles.root}>
@@ -25,22 +46,45 @@ const GalleryProduct = ({ setId, id, products }) => {
         <FontAwesomeIcon icon={faAngleLeft} />
       </Button>
       <div className={styles.slide}>
-        {products.slice(activePage * 6, (activePage + 1) * 6).map(item => {
-          const handleClick = e => {
-            e.preventDefault();
-            setId(item.id);
-          };
+        <Swiper
+          ref={sliderRef}
+          className='mySwiper'
+          slidesPerView={6}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            390: {
+              slidesPerView: 4,
+              spaceBetween: 10,
+            },
+            768: {
+              slidesPerView: 6,
+              spaceBetween: 10,
+            },
+          }}
+        >
+          {products.map(item => {
+            const handleClick = e => {
+              e.preventDefault();
+              setId(item.id);
+            };
 
-          return (
-            <div
-              key={item.id}
-              className={clsx(styles.slideItem, item.id !== id ? styles.overlay : '')}
-              onClick={handleClick}
-            >
-              <ProductImage {...item} />
-            </div>
-          );
-        })}
+            return (
+              <SwiperSlide key={item.id}>
+                <div
+                  className={clsx(
+                    styles.slideItem,
+                    item.id !== id ? styles.overlay : ''
+                  )}
+                  onClick={handleClick}
+                >
+                  <ProductImage {...item} />
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
       <Button className={styles.rightarrow} onClick={rightClick}>
         <FontAwesomeIcon icon={faAngleRight} />
